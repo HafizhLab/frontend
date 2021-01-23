5fa239ae384a595cce078788c39b9b428692db41
larif216 <muhamadlutfiarif10@gmail.com>
2021-01-23 23:03:55 +0700

    Setup ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/essential',
+        "plugin:vue/recommended",
+        "eslint:recommended",
+        "prettier/vue",
+        "plugin:prettier/recommended"
+    ],
+    // required to lint *.vue files
+    plugins: [
+        'vue'
+    ],
+    globals: {
+        $nuxt: true
+    },
+    // add your custom rules here
+    rules: {
+        parser: "babel-eslint"
+    }
 }
