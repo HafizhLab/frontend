cab2f49bc46fd84a2448462aebf31e5a1fbc0724
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2021-01-30 11:53:48 +0700

    Update title get from API question

--- pages/play/multiplayer/questions.vue
+++ pages/play/multiplayer/questions.vue
@@ -24,11 +24,8 @@
       </h3>
     </div>
     <div class="main mt-3 pt-4 pb-3 container">
-      <h5 v-if="$route.params.basedOn == 'Surah'">
-        <strong>{{ surah }}</strong>
-      </h5>
-      <h5 v-else>
-        <strong>Juz {{ surah }}</strong>
+      <h5>
+        <strong>{{ currentQuestion.title }}</strong>
       </h5>
       <div class="question-section pt-3 pb-3">
         <h3>{{ currentQuestion.text }}</h3>
