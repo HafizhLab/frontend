383a6ac5e559bfcafc324032076e38be3c53e375
hashlash <muh.ashlah@gmail.com>
2022-04-26 17:41:48 +0700

    Add root redirect to play

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="pt-3 pb-5 container text-center">
+      <h5>Redirecting...</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$router.push("/play");
+  },
+};
+</script>
