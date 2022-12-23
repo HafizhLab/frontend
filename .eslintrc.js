c855a1706bdcf1eee82f79cdfc029776fdee128f
hashlash <23237214+hashlash@users.noreply.github.com>
2022-12-23 09:51:41 +0000

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,29 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    "plugin:vue/essential",
     "plugin:vue/recommended",
     "eslint:recommended",
     "prettier/vue",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   globals: {
-    $nuxt: true
+    $nuxt: true,
   },
   // add your custom rules here
   rules: {
-    "no-console": "off"
-  }
-}
+    "no-console": "off",
+    "vue/multi-word-component-names": "off", // TODO: reconsider
+    "vue/no-reserved-component-names": "off", // TODO: reconsider
+  },
+};
